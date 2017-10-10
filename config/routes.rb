c9edb1bfbf3bacf3d2773ecc8a7ce318d0a5794c
raegertay <raegertay89@gmail.com>
2017-10-10 10:47:20 +0800

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+
+  # User keys in '/home'. to: <controller>#<action>
+  get '/home', to: 'pages#home'
+  get '/contactme', to: 'pages#contactme'
+end
